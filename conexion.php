423d66815a646b377c218c35a9153a7723ab4f2f
FernHB <75650598+FernHB@users.noreply.github.com>
2020-12-07 22:42:52 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-$mysqli=new mysqli("sql10.freesqldatabase.com","sql10380866","uQUC1DCMt9","sql10380866");
-if(mysqli_connect_errno()){
-	echo "Conexion Fallida:",mysqli_connect_errno();
-	exit();
-}
-
-?>
